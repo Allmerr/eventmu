7c951a4bf4efc1d0c6aa6674b3f5997204d72e60
Kevin Almer <kevinalmer4@gmail.com>
2023-10-18 16:36:04 +0700

    change factory

--- database/factories/FollowerFactory.php
+++ database/factories/FollowerFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Server;
 use App\Models\User;
+use App\Models\Follower;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Folower>
@@ -18,17 +19,29 @@ class FollowerFactory extends Factory
      */
     public function definition(): array
     {
-        $userId = $this->faker->randomElement(User::pluck('id')->all());
+        // cant follow own server
+        // cant follow twice the same server
 
-        $server = Server::find($this->faker->randomElement(Server::pluck('id')->all()));
+        $user =  User::find($this->faker->randomElement(User::pluck('id')->all()));
+        $server =  Server::find($this->faker->randomElement(Server::pluck('id')->all()));
 
-        while($server->user_id == $userId){
-            $server = Server::find($this->faker->randomElement(Server::pluck('id')->all()));
+        $isOwnServer = $user->id === $server->user_id;
+        $isAlreadyFollowed = Follower::where('user_id', $user->id)->where('server_id', $server->id)->exists();
+
+        while ($isOwnServer || $isAlreadyFollowed) {
+            $user =  User::find($this->faker->randomElement(User::pluck('id')->all()));
+            $server =  Server::find($this->faker->randomElement(Server::pluck('id')->all()));
+
+            $isOwnServer = $user->id === $server->user_id;
+            $isAlreadyFollowed = Follower::where('user_id', $user->id)->where('server_id', $server->id)->exists();
         }
 
-        return [
-            'server_id' => $server->id,
-            'user_id' => $userId,
-        ];
+        if(!$isOwnServer || !$isAlreadyFollowed){
+            return [
+                'user_id' => $user->id,
+                'server_id' => $server->id,
+            ];
+        }
+
     }
 }
--- resources/views/servers/index.blade.php
+++ resources/views/servers/index.blade.php
@@ -7,23 +7,19 @@
 
     <div class="row">
         @foreach ($servers as $server)
+            @if(!auth()->user()->IsMyOwnServer($server->id))
             <div class="col-md-4">
                 <div class="card mb-3">
                     <div class="card-body">
                         <h5 class="card-title">{{ $server->name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">#{{ $server->code }} by {{ $server->user->name }}</h6>
                         <p class="card-text">{{ Str::limit($server->description, 50) }}</p>
-                        @if(!auth()->user()->IsMyOwnServer($server->id))
-                        <a class="btn btn-primary" href="{{ route('servers.follow', $server->code) }}">Follow</a>
-                        @elseif(auth()->user()->hasFollowed($server->id))
                         <a class="btn btn-primary" href="{{ route('servers.follow', $server->code) }}">UnFollow</a>
-                        @endif
-
-
                         <a class="btn btn-primary" href="{{ route('servers.show', $server->code) }}">Detail</a>
                     </div>
                 </div>
             </div>
+            @endif
         @endforeach
     </div>
 
