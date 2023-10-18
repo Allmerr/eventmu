1a535ab46e7853ae7ec33dcfbe2c59c802debba8
Kevin Almer <kevinalmer4@gmail.com>
2023-10-18 20:57:05 +0700

    add is_deleted column in users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('is_deleted', ['0', '1'])->default('0');
             $table->timestamps();
         });
     }
